baf2aaae748f376457b81eb7d83388bee9bfa236
Zhang bao lin <59828992+bowling00@users.noreply.github.com>
2023-07-10 13:25:07 +0800

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,8 @@ module.exports = {
       name: 'devlink-server',
       script: 'npm',
       args: 'run start:prod',
+      watch: ['./dist'],
+      ignore_watch : ["node_modules"],
     },
   ],
 };
